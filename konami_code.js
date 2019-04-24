c3568506acd79bf3ffd32b91fd4e80fe80bb0ff1
whosAle <abelgrave95@gmail.com>
2019-04-24 09:15:16 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
+  const body = document.body;
+  let i = 0;
+  function konamiSequence(event) {
+
+    if (event.key === codes[i]) {
+      if (i == codes.length - 1) {
+        alert("Knoami Code!!!");
+        i = 0
+      } else {
+        i++;
+      }
+    } else {
+      i = 0;
+    };
+  };
+
+  body.addEventListener('keydown', konamiSequence);
 }
